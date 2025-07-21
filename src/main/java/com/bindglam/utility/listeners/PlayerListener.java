f086467c5659829e0f6782f85342e5a90be31615
Woobeen Jeon <woobeen0516@gmail.com>
2025-07-21 18:03:57 +0900

    hotfix

--- src/main/java/com/bindglam/utility/listeners/PlayerListener.java
+++ src/main/java/com/bindglam/utility/listeners/PlayerListener.java
@@ -9,19 +9,28 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+import java.util.Objects;
 
 public class PlayerListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onInventoryClose(InventoryCloseEvent event) {
         Player player = (Player) event.getPlayer();
         Inventory inventory = event.getInventory();
+        InventoryHolder holder = inventory.getHolder(false);
 
         BindglamInventoryCloseEvent closeEvent = new BindglamInventoryCloseEvent(event.getView(), event.getReason());
 
         closeEvent.callEvent();
 
         if(closeEvent.isCancelled() && event.getReason() != InventoryCloseEvent.Reason.DEATH && event.getReason() != InventoryCloseEvent.Reason.DISCONNECT) {
-            Bukkit.getScheduler().runTaskLater(BindglamUtility.getInstance(), () -> player.openInventory(inventory), 1L);
+            Bukkit.getScheduler().runTaskLater(BindglamUtility.getInstance(), () -> {
+                if(holder != null)
+                    player.openInventory(holder.getInventory());
+                else
+                    player.openInventory(inventory);
+            }, 1L);
         }
     }
 }
