7de9fdc16d62911fd6e7eeb054b4f29926f697e9
Woobeen Jeon <woobeen0516@gmail.com>
2025-07-25 20:48:49 +0900

    리펙토링

--- core/src/main/java/com/bindglam/utility/database/SQLiteDatabase.java
+++ core/src/main/java/com/bindglam/utility/database/SQLiteDatabase.java
@@ -8,6 +8,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLiteDatabase implements Database {
+    private static final int VALID_TIMEOUT = 500;
+
     private Connection connection;
 
     @Override
@@ -19,7 +21,7 @@ public class SQLiteDatabase implements Database {
         }
 
         try {
-            connection = DriverManager.getConnection("jdbc:sqlite:plugins/BindglamUtility/database.db");
+            createConnection();
         } catch (SQLException e) {
             throw new RuntimeException("Failed to load database", e);
         }
@@ -40,10 +42,22 @@ public class SQLiteDatabase implements Database {
 
     @Override
     public @NotNull Connection getConnection() {
+        try {
+            if(connection == null || connection.isValid(VALID_TIMEOUT)) {
+                close();
+                createConnection();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to ensure connection", e);
+        }
         return connection;
     }
 
     @Override
     public void evictConnection(@NotNull Connection connection) {
+    }
+
+    private void createConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:sqlite:plugins/BindglamUtility/database.db");
     }
 }
