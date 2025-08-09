11332fc0be4bd42e2eaef482c0be36f0c9c6e4d3
Woobeen Jeon <woobeen0516@gmail.com>
2025-08-09 16:26:42 +0900

    HikariCP 6.3.2 -> 7.0.1

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     implementation("dev.jorel:commandapi-bukkit-shade:10.1.2")
     implementation("com.alibaba.fastjson2:fastjson2:2.0.58")
-    implementation("com.zaxxer:HikariCP:6.3.2")
+    implementation("com.zaxxer:HikariCP:7.0.1")
 
     rootProject.project("nms").subprojects.forEach {
         implementation(project(":nms:${it.name}", configuration = "reobf"))
