2127b2fd1a2e5a5a42c06a68fb795d0b863fad54
Woobeen Jeon <woobeen0516@gmail.com>
2025-08-09 09:23:59 +0900

    fastjson2 2.0.57 -> 2.0.58

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     implementation(project(":api"))
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     implementation("dev.jorel:commandapi-bukkit-shade:10.1.2")
-    implementation("com.alibaba.fastjson2:fastjson2:2.0.57")
+    implementation("com.alibaba.fastjson2:fastjson2:2.0.58")
     implementation("com.zaxxer:HikariCP:6.3.2")
 
     rootProject.project("nms").subprojects.forEach {
